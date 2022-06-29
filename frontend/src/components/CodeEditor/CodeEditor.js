c574b6339c17f011e4f80e25993b91c36261da9c
Colin Lin <colincool100@gmail.com>
2022-06-29 15:03:03 -0400

    Revert "fix race condition"
    This reverts commit 46ee50152b8b0b2b2e8ec927496b6118c0a21d51.

--- frontend/src/components/CodeEditor/CodeEditor.js
+++ frontend/src/components/CodeEditor/CodeEditor.js
@@ -20,7 +20,6 @@ const doc = connection.get(collection, id);
 
 function CodeEditor() {
   const [pageContent, setPageContent] = useState({});
-  let existingPromise = Promise.resolve();
 
   useEffect(() => {
     doc.subscribe((error) => {
@@ -31,22 +30,10 @@ function CodeEditor() {
       });
     });
   }, []);
-  const handleChange = async (content, delta, source, editor) => {
+  const handleChange = (content, delta, source, editor) => {
     if (source !== "user") return;
     console.log(editor.getContents());
-    await existingPromise;
-    const submitOpPromise = () => {
-      return new Promise((resolve, reject) => {
-        doc.submitOp(editor.getContents(), (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve("ok");
-          }
-        });
-      });
-    };
-    submitOpPromise();
+    doc.submitOp(editor.getContents());
   };
   hljs.registerLanguage("javascript", javascript);
   hljs.registerLanguage("python", python);
